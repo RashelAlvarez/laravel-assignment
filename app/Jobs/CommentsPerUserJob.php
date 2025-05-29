247997da8af7f65465e41453a6756b91b9bfe949
RashelAlvarez <ralvarez@intelix.biz>
2025-05-29 01:14:38 -0400

    add log format CommentsPerUserJob

--- app/Jobs/CommentsPerUserJob.php
+++ app/Jobs/CommentsPerUserJob.php
@@ -8,17 +8,21 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class CommentsPerUserJob implements ShouldQueue
 {
     use Queueable;
     protected $commentCountService;
+    protected $uuid;
+
     /**
      * Create a new job instance.
      */
     public function __construct(CommentCountInterface $commentCountService)
     {
         $this->commentCountService = $commentCountService;
+        $this->uuid = Str::uuid();
     }
 
     /**
@@ -27,12 +31,17 @@ class CommentsPerUserJob implements ShouldQueue
     public function handle(): void
     {
         //
-
         $userCommentCounts = $this->commentCountService->getCommentCounts();
         if (empty($userCommentCounts)) {
             Log::warning('No se encontraron comentarios para exportar.');
         }
-        Log::info('User comment counts calculated successfully.', ['counts' => $userCommentCounts]);
+        $timestamp = now()->toIso8601String();
+
+        foreach ($userCommentCounts as $user) {
+            Log::channel('single')->info("{$timestamp} - CommentsPerUserJob[{$this->uuid}] Processed comment counts for user_id={$user['user_id']}: total={$user['comment_count']}");
+        }
+
+        #Log::info('User comment counts calculated successfully.', ['counts' => $userCommentCounts]);
         Storage::disk('local')->put('user_comment_counts.json', json_encode($userCommentCounts));
     }
 }
