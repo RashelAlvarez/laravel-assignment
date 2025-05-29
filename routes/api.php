9da5cca16511a6c37689efa00b5dab02d45102a5
RashelAlvarez <ralvarez@intelix.biz>
2025-05-28 22:16:48 -0400

    add readme.md

---
+++ routes/api.php
@@ -0,0 +1,7 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+#Route::get('/v1/users', [UserController::class, 'index']);
+#Route::get('/v1/users/{id}', [UserController::class, 'show']);
