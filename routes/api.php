7cd4198d0b405a4258d075c5b5c3cff84b0dea9b
RashelAlvarez <ralvarez@intelix.biz>
2025-05-28 22:20:18 -0400

    first_commit

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,3 @@
 
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
-
-#Route::get('/v1/users', [UserController::class, 'index']);
-#Route::get('/v1/users/{id}', [UserController::class, 'show']);
